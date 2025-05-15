14f32cd279fc7cf53bbc5fde9ec901b3aa617315
Erick Castillo <erickcastillo@uxlsoftware.com>
2025-05-14 23:05:30 -0400

    update

--- UltimateHoopers/Converter/StringNotEmptyConverter.cs
+++ UltimateHoopers/Converter/StringNotEmptyConverter.cs
@@ -1,30 +1,20 @@
-﻿using System;
+﻿
+using System;
 using System.Globalization;
 using Microsoft.Maui.Controls;
 
 namespace UltimateHoopers.Converters
 {
-    /// <summary>
-    /// Converts a string URL to an ImageSource for binding in XAML
-    /// </summary>
-    public class StringToImageSourceConverter : IValueConverter
+    public class StringNotEmptyConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is string imageUrl && !string.IsNullOrWhiteSpace(imageUrl))
+            if (value is string stringValue)
             {
-                try
-                {
-                    return ImageSource.FromUri(new Uri(imageUrl));
-                }
-                catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine($"Error converting URL to ImageSource: {ex.Message}");
-                    return null;
-                }
+                return !string.IsNullOrWhiteSpace(stringValue);
             }
 
-            return null;
+            return false;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
